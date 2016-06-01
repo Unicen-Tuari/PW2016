d78e695fa06c39cb16168d1c2f085b4a03bee0de
Ignacio Jonas <ignaciojonas@gmail.com>
2016-06-01 20:34:46 -0300

    REST Obtener info del grupo

--- docentes/Ajax/REST/js/information.js
+++ docentes/Ajax/REST/js/information.js
@@ -23,3 +23,32 @@ function guardarInformacion() {
   });
 }
 
+function mostrarInfo(info) {
+  var information="";
+  for (var i = 0; i < info.length; i++) {
+    information+=info[i].group+" "+info[i].thing;
+  }
+  return information;
+}
+
+function getInformationByGroup(){
+  event.preventDefault();
+  var groupid=$('#groupid').val();
+  $.ajax({
+    url:"http://web-unicen.herokuapp.com/api/group/" + groupid,
+    method:"GET",
+    contentType: "application/json; charset=utf-8",
+    success: function(resultData){
+      if(resultData.information.length == 0){
+        $('#infoGroup').html("No hay info para este grupo");
+        return;
+      }
+      $('#infoGroup').html(mostrarInfo(resultData.information));
+      console.log(resultData.information[0].thing);
+    },
+    error:function(jqxml, status, errorThrown){
+      alert('Error!');
+      console.log(errorThrown);
+    }
+  });
+}
